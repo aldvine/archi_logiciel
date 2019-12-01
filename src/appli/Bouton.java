b86d7bc729518e12b3ce892e5a46c178a877fa0a
matt <matgonzalez736@gmail.com>
2019-12-01 18:32:59 +0100

    ajout liaison button -method afficher modifie

---
+++ src/appli/Bouton.java
@@ -0,0 +1,48 @@
+package appli;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+
+public class Bouton extends JButton implements MouseListener {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private String name;
+
+    public Bouton(String str) {
+        super(str);
+        this.name = str;
+        this.addMouseListener(this);
+    }
+
+    public void mousePressed(MouseEvent event) {
+
+    }
+
+    public void mouseClicked(MouseEvent event) {
+        // Inutile d'utiliser cette méthode ici
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
--- src/appli/Main.java
+++ src/appli/Main.java
@@ -2,17 +2,16 @@ package appli;
 
 public class Main {
 
-	//1 extract method du sysout --> affiche() 
-	//2 Create class Afficheur
-	//3 d�placer la m�thode affiche() -> Afficheur
-	//4 add Field Afficheur to Appli
-	//5 source -> generateDelegateMethod affiche()
-	//6 Sur Afficheur clic droit -> refactor -> Extrat interface
+	// 1 extract method du sysout --> affiche()
+	// 2 Create class Afficheur
+	// 3 d�placer la m�thode affiche() -> Afficheur
+	// 4 add Field Afficheur to Appli
+	// 5 source -> generateDelegateMethod affiche()
+	// 6 Sur Afficheur clic droit -> refactor -> Extrat interface
 	public static void main(String[] args) throws Exception {
-		
-		Appli a  = new Appli();
-		a.run();
-	
+		Appli app = new Appli();
+		Fenetre ihm = new Fenetre(app);
+		ihm.start();
 	}
-	
+
 }
